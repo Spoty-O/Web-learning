278f7c0e40ca5064aea65cda616a5bd67e1da4c7
Spoty-O <korotysh16@ukr.net>
2024-08-04 15:20:53 +0300

    Handlers extension

--- server/src/controllers/UserController.ts
+++ server/src/controllers/UserController.ts
@@ -1,14 +1,46 @@
 import { Request, Response, NextFunction } from "express";
 import ApiError from "../helpers/ApiErrors";
 import { User } from "../models/UserModel";
+import { CreationUserAttributes } from "../types/UserTypes";
 
 class UserController {
-  static async get_user(req: Request, res: Response, next: NextFunction) {
+  static async get_user(
+    req: Request<undefined, undefined, { id: number }>,
+    res: Response,
+    next: NextFunction
+  ) {
     try {
-        return res.send("Hello on my page!");
+      const { id } = req.body;
+      const user = await User.findByPk(id);
+      if (!user) {
+        return next(ApiError.notFound("User not found."));
+      }
+      return res.json(user);
     } catch (error) {
-        console.log(error)
-        return ApiError.internal("Server error!")
+      console.log(error);
+      return next(ApiError.internal("Server error!"));
+    }
+  }
+
+  static async create_user(
+    req: Request<undefined, undefined, CreationUserAttributes>,
+    res: Response,
+    next: NextFunction
+  ) {
+    try {
+      const { email, login, password } = req.body;
+      console.log(req.body);
+      if (await User.findOne({ where: { email: email } })) {
+        return next(ApiError.conflict("User already exists."));
+      }
+      const user = await User.create(req.body);
+      if (!user) {
+        return next(ApiError.internal("User account creation error."));
+      }
+      return res.json(user);
+    } catch (error) {
+      console.log(error);
+      return next(ApiError.internal("Server error!"));
     }
   }
 }
--- server/src/routes/userRoutes.ts
+++ server/src/routes/userRoutes.ts
@@ -4,5 +4,6 @@ import UserController from "../controllers/UserController";
 const routes = Router();
 
 routes.get("/", UserController.get_user);
+routes.post("/", UserController.create_user)
 
 export default routes;
